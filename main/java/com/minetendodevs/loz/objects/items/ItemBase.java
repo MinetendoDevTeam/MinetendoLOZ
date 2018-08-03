2fc8da7f253d0c1c025e12e531c6f110b6e2f112
MinetendoDevTeam <42070254+MinetendoDevTeam@users.noreply.github.com>
2018-08-03 10:50:11 -0400

    Add files via upload

---
+++ main/java/com/minetendodevs/loz/objects/items/ItemBase.java
@@ -0,0 +1,26 @@
+package com.minetendodevs.loz.objects.items;
+
+import com.minetendodevs.loz.Main;
+import com.minetendodevs.loz.init.ModItems;
+import com.minetendodevs.loz.util.IHasModel;
+
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item implements IHasModel {
+
+	public ItemBase(String name)
+	{
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(Main.minetendotab);
+		
+		ModItems.ITEMS.add(this);
+	}
+	
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this,  0,  "inventory");
+	}
+
+}
